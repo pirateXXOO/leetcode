cdb987f8237b28759869db3e1c83932dbc501726
shuai Wang <shuaiwang@bjyz-dl-001.autox.sz>
2022-04-27 13:51:10 +0800

    fizzBuzz

--- leetcode.go
+++ leetcode.go
@@ -1656,3 +1656,22 @@ package main
 // *func (this *MinStack) GetMin() int {
 // *	return this.mins[len(this.mins)-1]
 // *}
+
+////////// fizzBuzz
+// https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xngt85/
+
+// func fizzBuzz(n int) []string {
+// 	var res []string
+// 	for i:=1;i<= n ; i++{
+// 		if i % 3 == 0 && i % 5 == 0 {
+// 			res = append(res, "FizzBuzz")
+// 		} else if i % 3 == 0 {
+// 			res = append(res, "Fizz")
+// 		} else if i %5 ==0 {
+// 			res = append(res, "Buzz")
+// 		} else {
+// 			res = append(res, strconv.Itoa(i))
+// 		}
+// 	}
+// 	return res
+// }
