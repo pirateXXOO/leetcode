20c680feef0464c679d67a6e4d3be46d539ed8e8
shuai Wang <shuaiwang@bjyz-dl-001.autox.sz>
2022-04-29 10:17:50 +0800

    threeSum stage two

--- leetcode_test.go
+++ leetcode_test.go
@@ -1,26 +1,22 @@
 package main
 
-import (
-	"testing"
-)
-
-func TestIfEuqal(t *testing.T) {
-	if !IfEqual([]int{1, 2, 3}, []int{3, 2, 1}) {
-		t.Errorf("TestFailed")
-	}
-	if !IfEqual([]int{1, -1, 0}, []int{1, 0, -1}) {
-		t.Errorf("TestFailed")
-	}
-	if !IfEqual([]int{-1, 2, -1}, []int{2, -1, -1}) {
-		t.Errorf("TestFailed")
-	}
-	if !IfEqual([]int{0, -3, 3}, []int{-3, 3, 0}) {
-		t.Errorf("TestFailed")
-	}
-	if !IfEqual([]int{-1, -2, 3}, []int{-1, -2, 3}) {
-		t.Errorf("TestFailed")
-	}
-}
+// func TestIfEuqal(t *testing.T) {
+// 	if !IfEqual([]int{1, 2, 3}, []int{3, 2, 1}) {
+// 		t.Errorf("TestFailed")
+// 	}
+// 	if !IfEqual([]int{1, -1, 0}, []int{1, 0, -1}) {
+// 		t.Errorf("TestFailed")
+// 	}
+// 	if !IfEqual([]int{-1, 2, -1}, []int{2, -1, -1}) {
+// 		t.Errorf("TestFailed")
+// 	}
+// 	if !IfEqual([]int{0, -3, 3}, []int{-3, 3, 0}) {
+// 		t.Errorf("TestFailed")
+// 	}
+// 	if !IfEqual([]int{-1, -2, 3}, []int{-1, -2, 3}) {
+// 		t.Errorf("TestFailed")
+// 	}
+// }
 
 // func TestThreeSum(t *testing.T) {
 // 	ans := ThreeSum([]int{-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4})
